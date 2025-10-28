3e72775d657eba7955cc5a87ac099def053f9240
Valiko10 <javlondaletov@gmail.com>
2025-10-28 16:32:51 +0500

    yangi rekursiv funksiya

---
+++ github/rekursiv_funksiya_uchun_misollar.py
@@ -0,0 +1,39 @@
+import math
+# def factorial_top(n):
+    
+#     if n == 0 :
+#         return 1
+#     elif n != 0 :
+#         return math.factorial(n)
+# print(factorial_top(5))
+
+
+# def fibonanchi(n):
+#     if n == 1 or n == 2 :
+#         return 1
+#     else:
+#         return fibonanchi(n-1) + fibonanchi(n-2)
+# print(fibonanchi(5))
+
+# 3
+# def raqamlar_yigindisi(n):
+#     if n < 10 :
+#         return n
+#     else:
+#         return n % 10 + raqamlar_yigindisi(n//10)
+
+
+# 4 
+# def ohirgi_son(n):
+#     if n <= 10 :
+#         return n
+#     else:
+#         return n % 10
+# print(ohirgi_son(35))
+
+# 5
+# def raqamlar_soni(n):
+#     if n == 1 :
+#         return n
+#     elif n == 
+
