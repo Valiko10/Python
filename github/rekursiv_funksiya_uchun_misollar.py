dd85005ca8a3ce3d2897dab163a13b3ef8990d3e
Valiko10 <javlondaletov@gmail.com>
2025-10-30 16:04:48 +0500

    '10.30.2025'

--- github/rekursiv_funksiya_uchun_misollar.py
+++ github/rekursiv_funksiya_uchun_misollar.py
@@ -33,7 +33,31 @@ import math
 
 # 5
 # def raqamlar_soni(n):
-#     if n == 1 :
-#         return n
-#     elif n == 
+    # if n == 1 :
+        # return n
+    # else:
+        # return 1 + raqamlar_soni(n // 10)
+# print(raqamlar_soni(145))
+
+# 6
+# def teskarilash(matn):
+#     if len(matn) == 0 :
+#         return " "
+#     else :
+#         return matn[-1] + teskarilash(matn[:-1])
+# print(teskarilash('ulugbek'))
+
+# 7
+# def royhat_yigindisi(royhat):
+#     if len(royhat) == 0 :
+#         return 0
+#     else: 
+#         return royhat[0] + royhat_yigindisi(royhat[1:])
+# print(royhat_yigindisi([10,20,30,40,50]))
+
+# 9
+
+
+
+
 
